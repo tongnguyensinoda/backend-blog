443c567284bee97e906959c2b7ded8634682e8eb
tongnguyensinoda <lamnguyensinoda@gmail.com>
2022-11-11 09:20:50 +0700

    update Main and Create constants

--- blog-apis/blog-apis/src/main/java/com/tong/rmit/blogapis/BlogApisApplication.java
+++ blog-apis/blog-apis/src/main/java/com/tong/rmit/blogapis/BlogApisApplication.java
@@ -1,13 +1,17 @@
 package com.tong.rmit.blogapis;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class BlogApisApplication {
-
+public class BlogApisApplication implements CommandLineRunner {
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApisApplication.class, args);
 	}
@@ -15,5 +19,10 @@ public class BlogApisApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.passwordEncoder.encode("xyz"));
 	}
 }
